5d10045a20615db17efc8c789c104133e14092c1
Alan Thompson <cloojure@gmail.com>
2019-03-28 10:23:47 -0700

    misc

--- test/clj/tst/demo/core.clj
+++ test/clj/tst/demo/core.clj
@@ -10,29 +10,3 @@
 
 
 
-(defn jsonista-round-trip [arg]
-  (it-> arg
-    (json/write-value-as-string it)
-    (json/read-value it)))
-
-(dotest
-  (spyx (jsonista-round-trip {:kikka 42}))
-  (spyx (jsonista-round-trip {"one" 1
-                              ""    {:kikka 42}})))
-
-(comment
-  ; result
-  (jsonista-round-trip {:kikka 42}) => {"kikka" 42}
-  (jsonista-round-trip {"one" 1, "" {:kikka 42}}) => {"" {"kikka" 42}, "one" 1}
-)
-
-(defn muun-round-trip [arg]
-  (it-> arg
-    (muun/encode tp/application-json it)
-    (muun/decode tp/application-json it)))
-
-(dotest
-  (spyx (muun-round-trip {:kikka 42}))
-  (spyx (muun-round-trip {"one" 1
-                          ""    {:kikka 42}})))
-
