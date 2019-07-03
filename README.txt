b38a6464ccf7a0aeacc261d8903cd72b986157df
Alan Thompson <cloojure@gmail.com>
2019-07-02 17:47:34 -0700

    misc

---
+++ README.txt
@@ -0,0 +1,10 @@
+
+build container:
+
+  docker build -t demo .
+
+run container:
+
+  docker run --rm demo
+
+
--- test/clj/tst/demo/core.clj
+++ test/clj/tst/demo/core.clj
@@ -1,18 +1,5 @@
 (ns tst.demo.core
-  (:use demo.core tupelo.core tupelo.test)
-  (:import [java.time ZonedDateTime ZoneId]))
+  (:use demo.core tupelo.core tupelo.test))
 
-(defn inst->date-time
-  "Convert a java.time.Instant to a DateTime for the supplied ZoneId"
-  [instant zoneid]
-  (.toLocalDate
-    (ZonedDateTime/ofInstant instant zoneid)))
 
-(dotest
-  (let [may-4    #inst "2018-05-04T01:23:45.678-00:00" ; a java.util.Date
-        instant  (.toInstant may-4) ]
-    (spyxx may-4)
-    (spyx instant)
-    (println "utc =>" (inst->date-time instant (ZoneId/of "UTC")))
-    (println "nyc =>" (inst->date-time instant (ZoneId/of "America/New_York")))
-    ))
+
