90fa9429c77a8e242b8e6d13d81620d9deb485b7
Alan Thompson <cloojure@gmail.com>
2019-10-02 15:05:18 -0700

    misc

--- test/clj/tst/demo/prof.clj
+++ test/clj/tst/demo/prof.clj
@@ -1,4 +1,4 @@
-(ns tst.demo.core
+(ns tst.demo.prof
   (:use tupelo.core tupelo.test)
   (:require
     [demo.prof :as prof :refer [defnp]]
@@ -33,7 +33,7 @@
 
     ; -----------------------------------------------------------------------------
     ; Sample value for (prof/stats-get-all)
-    ;   #:tst.demo.core{:sleep-2 {:n 13, :mean 0.002509983, :sigma 0.000127452537},
+    ;   #:tst.demo.prof{:sleep-2 {:n 13, :mean 0.002509983, :sigma 0.000127452537},
     ;                   :sleep-3 {:n 13, :mean 0.003584854, :sigma 0.000274577250},
     ;                   :sleep-7 {:n 11, :mean 0.007849257, :sigma 0.000651772415},
     ;                   :sleep-5 {:n 12, :mean 0.005633091, :sigma 0.000442189072},
@@ -43,25 +43,25 @@
     ;                   :sleep-77 {:n 3, :mean 0.079445910, :sigma 0.000089636781}}
     ; -----------------------------------------------------------------------------
     ; Sample output for (prof/stats-print-all)
-    ;   :tst.demo.core/sleep-2       13   0.00247 0.00015098
-    ;   :tst.demo.core/sleep-3       13   0.00364 0.00020310
-    ;   :tst.demo.core/sleep-5       12   0.00570 0.00048202
-    ;   :tst.demo.core/sleep-7       11   0.00761 0.00029422
-    ;   :tst.demo.core/sleep-11       9   0.01250 0.00088938
-    ;   :tst.demo.core/sleep-13       9   0.01464 0.00114652
-    ;   :tst.demo.core/sleep-17       8   0.01938 0.00127845
-    ;   :tst.demo.core/sleep-77       3   0.07960 0.00099748
+    ;   :tst.demo.prof/sleep-2       13   0.00247 0.00015098
+    ;   :tst.demo.prof/sleep-3       13   0.00364 0.00020310
+    ;   :tst.demo.prof/sleep-5       12   0.00570 0.00048202
+    ;   :tst.demo.prof/sleep-7       11   0.00761 0.00029422
+    ;   :tst.demo.prof/sleep-11       9   0.01250 0.00088938
+    ;   :tst.demo.prof/sleep-13       9   0.01464 0.00114652
+    ;   :tst.demo.prof/sleep-17       8   0.01938 0.00127845
+    ;   :tst.demo.prof/sleep-77       3   0.07960 0.00099748
     ; -----------------------------------------------------------------------------
     (let [stats-all (prof/stats-get-all)]
       (let [within-tol (fn [val low] (<= low val (+ low 0.005)))]
-        (is (within-tol (fetch-in stats-all [:tst.demo.core/sleep-2 :mean]) 0.002))
-        (is (within-tol (fetch-in stats-all [:tst.demo.core/sleep-3 :mean]) 0.003))
-        (is (within-tol (fetch-in stats-all [:tst.demo.core/sleep-5 :mean]) 0.005))
-        (is (within-tol (fetch-in stats-all [:tst.demo.core/sleep-7 :mean]) 0.007))
-        (is (within-tol (fetch-in stats-all [:tst.demo.core/sleep-11 :mean]) 0.011))
-        (is (within-tol (fetch-in stats-all [:tst.demo.core/sleep-13 :mean]) 0.013))
-        (is (within-tol (fetch-in stats-all [:tst.demo.core/sleep-17 :mean]) 0.017))
-        (is (within-tol (fetch-in stats-all [:tst.demo.core/sleep-77 :mean]) 0.077)))
+        (is (within-tol (fetch-in stats-all [:tst.demo.prof/sleep-2 :mean]) 0.002))
+        (is (within-tol (fetch-in stats-all [:tst.demo.prof/sleep-3 :mean]) 0.003))
+        (is (within-tol (fetch-in stats-all [:tst.demo.prof/sleep-5 :mean]) 0.005))
+        (is (within-tol (fetch-in stats-all [:tst.demo.prof/sleep-7 :mean]) 0.007))
+        (is (within-tol (fetch-in stats-all [:tst.demo.prof/sleep-11 :mean]) 0.011))
+        (is (within-tol (fetch-in stats-all [:tst.demo.prof/sleep-13 :mean]) 0.013))
+        (is (within-tol (fetch-in stats-all [:tst.demo.prof/sleep-17 :mean]) 0.017))
+        (is (within-tol (fetch-in stats-all [:tst.demo.prof/sleep-77 :mean]) 0.077)))
       (let [sigma-vals (forv [[tag stats] stats-all]
                          (grab :sigma stats))
             pass-flgs  (mapv #(< % 0.002) sigma-vals)]
