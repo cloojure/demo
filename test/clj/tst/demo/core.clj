58cd709061a934fa72038bdc3c3eee2612907bb2
Alan Thompson <cloojure@gmail.com>
2020-03-25 18:30:58 -0700

    misc

--- test/clj/tst/demo/core.clj
+++ test/clj/tst/demo/core.clj
@@ -1,6 +1,3 @@
-; #TODO  In a real app, would split up ns like:
-; #TODO         tic-tac-toe.core   - core fns
-; #TODO     tst.tic-tac-toe.core   - testing fns
 (ns tst.demo.core
   (:use tupelo.core tupelo.test)
   (:require
@@ -9,97 +6,75 @@
     [tupelo.core :as t]
     [tupelo.schema :as tsk]))
 
-(s/defn valid-elem? :- s/Bool
-  "Returns true iff element is valid from #{ :x :o nil }"
-  [elem]
-  (contains? #{:x :o nil} elem))
+(def Key s/Int)
 
-(s/defn diagonal-main :- tsk/Vec
-  "Returns the main diagonal of an array"
-  [arr :- ta/Array]
-  (assert (= (ta/num-rows arr)
-            (ta/num-cols arr)))
-  (let [rows-indexed (t/indexed arr)]
-    (t/forv [[idx row] rows-indexed]
-      (ta/elem-get arr idx idx))))
+(def nrows 3)
+(def ncols 3)
+(def nkeys (* nrows ncols))
+(def keys-vec (vec (take nkeys (thru 1 nkeys))))
+(def keys-set (set keys-vec))
 
-(s/defn diagonal-anti :- tsk/Vec
-  "Returns the anti-diagonal of an array"
-  [arr :- ta/Array]
-  (diagonal-main (ta/rotate-left arr)))
+(s/def keys-array :- ta/Array
+  (ta/row-vals->array nrows ncols keys-vec))
 
-(s/defn triple->winner :- s/Keyword
-  "Given a list of triples like [:x :o :x], return a list of
-     :x      - if X wins
-     :y      - if Y wins
-     ::none  - otherwise "
-  [row :- tsk/Triple]
-  (cond
-    (= [:x :x :x] row) :x
-    (= [:o :o :o] row) :o
-    :else ::none))
+(s/def key->coord :- {s/Int tsk/Pair}
+  (apply glue (sorted-map)
+    (for [ii (range nrows)
+          jj (range ncols)]
+      (let [key-val (ta/elem-get keys-array ii jj)]
+        {key-val [ii jj]}))))
 
-(defn board->result
-  "Determine if a completed tic-tac-toe board contains a winner.  Returns:
-     :x or :y    - if winner found
-     :cats-game  - otherwise. "
-  [board]
-  (let [arr (ta/rows->array board)] ; construct canonical array, verify rectangular
-    (assert (and ; verify valid shape 3x3
-              (= 3 (ta/num-rows arr))
-              (= 3 (ta/num-cols arr))))
-    (doseq [elem (ta/array->row-vals arr)] ; validate all elements
-      (t/validate valid-elem? elem))
-    (let [diag-main      (diagonal-main arr)
-          diag-anti      (diagonal-main (ta/rotate-left arr))
-          all-triples    (-> (t/glue
-                               (ta/array->rows arr)
-                               (ta/array->cols arr))
-                           (t/append diag-main)
-                           (t/append diag-anti))
-          winners        (mapv triple->winner all-triples)
-          winners-unique (set
-                           (remove #(= % ::none) winners))]
-      (cond
-        (zero? (count winners-unique)) :cats-game ; no winner
+(s/defn aligned? :- s/Bool
+  "Returns true if 3 points fall on a straight line"
+  [a :- Key
+   b :- Key
+   c :- Key]
+  (assert (= 3 (count #{a b c}))) ; ensure all unique
+  (let [[x1 y1] (grab a key->coord)
+        [x2 y2] (grab b key->coord)
+        [x3 y3] (grab c key->coord)
 
-        (= 1 (count winners-unique)) (first winners-unique) ; a single winner
+        dx2     (- x2 x1)
+        dx3     (- x3 x1)
+        dy2     (- y2 y1)
+        dy3     (- y3 y1)
 
-        :else ;  multiple winners found
-        (throw (ex-info "Multiple winners found! " (t/vals->map arr winners)))))))
+        prod-32 (* dx3 dy2)
+        prod-23 (* dx2 dy3)
+        result (t/rel= prod-32 prod-23 :digits 8) ]
+    result) )
 
 (dotest
-  ; demonstrate diag fns
-  (let [linear-array (ta/row-vals->array 3 3 [0 1 2
-                                              3 4 5
-                                              6 7 8])]
-    (is= [0 4 8] (diagonal-main linear-array))
-    (is= [2 4 6] (diagonal-anti linear-array)))
+  (is= keys-array [[1 2 3] ; what a 3x3 keypad looks like
+                   [4 5 6]
+                   [7 8 9]])
 
-  (is= :x (board->result [[:x :o :x] ; col win
-                          [:x :o :o]
-                          [:x :x :o]]))
-  (is= :x (board->result [[:x :x :x] ; row win
-                          [:o :x :o]
-                          [:x :o :o]]))
-  (is= :o (board->result [[:o :x :x] ; diag win
-                          [:x :o :x]
-                          [:x :o :o]]))
-  (is= :o (board->result [[:x :x :o] ; anti-diag win
-                          [:x :o :x]
-                          [:o :x :o]]))
+  (is= key->coord ; convert key value to [i,j] coords
+    {1 [0 0]
+     2 [0 1]
+     3 [0 2]
+     4 [1 0]
+     5 [1 1]
+     6 [1 2]
+     7 [2 0]
+     8 [2 1]
+     9 [2 2]})
 
-  (is= :cats-game (board->result [[:x :o :x]
-                                  [:x :o :x]
-                                  [:o :x :o]]))
+  (is (aligned? 1 2 3)) ; verify alishment tests on a 3x3 keypad
+  (is (aligned? 7 5 3))
+  (is (aligned? 7 1 4))
+  (is (aligned? 9 1 5))
+  (isnt (aligned? 8 1 5))
+  (isnt (aligned? 1 6 7))
+  (throws? (aligned? 0 6 7)) ; throws for invalid keys
+  (throws? (aligned? 0 6 10))
 
-  ; #awt #todo:  maybe need to identify and exclude incomplete games ???
-  (is= :cats-game (board->result [[:o :x :x]
-                                  [:x nil :x]
-                                  [:x :o :o]]))
 
-  (throws? (board->result [[:x :o :x] ; invalid result with multiple winners
-                           [:x :o :x]
-                           [:x :o :o]]))
 
   )
+;(is (valid-path [1 6 7 4]))   ;; knights jump is valid
+;(is (valid-path [2 1 3]))     ;; 2 is already used, so we can cross it
+;(isnt (valid-path [1 3 2]))     ;; can't get from 1 to 3 without using 2 first
+;(isnt (valid-path [1 9]))       ;; can't cross 5 without using
+;(isnt (valid-path [1 2 3 2 1])) ;; can't use dots more than once
+;(isnt (valid-path [0 1 2 3]))   ;; there's no dot 0
