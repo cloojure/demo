218a689375ba494122979e61c51b7bbbcaf94ba2
Alan Thompson <cloojure@gmail.com>
2020-05-05 13:49:18 -0700

    misc

--- test/clj/tst/demo/core.clj
+++ test/clj/tst/demo/core.clj
@@ -1,15 +1,5 @@
-;(ns tst.demo.core
-;  (:use demo.core tupelo.core tupelo.test)
-;  (:require
-;    [schema.core :as s] ))
-
 (ns tst.demo.core
   (:use demo.core tupelo.core tupelo.test)
-  (:import [demo Msg Homer Bart]))
-
-(dotest
-  (let [homer (Homer.)
-        bart  (Bart.)]
-    (spyx (.msg homer))
-    (spyx (.msg bart))))
+  (:require
+    [schema.core :as s] ))
 
